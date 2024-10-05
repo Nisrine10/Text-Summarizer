83eef3eb037815191110e51c0daca863e31a6019
Nisrine10 <aomarinissrine2001@gmail.com>
2024-10-05 17:45:35 +0100

    adding requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+with open("README.md","r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ ="0.0.0"
+REPO_NAME = ""
+AUTHOR_USER_NAME = "nisrine"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL="aomarinissrine2001@gmail.com"
